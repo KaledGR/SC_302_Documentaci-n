0e08dfb74bbf85e76a0467835c83f6de599c5b55
KaledGR <dgonzalez00893@ufide.ac.cr>
2023-09-18 20:54:16 -0600

    cambio a main
    cambio

--- mi-archivo.js
+++ mi-archivo.js
@@ -2,3 +2,4 @@ Alert("Hola mundo editado");
 Console.log("Cambios en el archivo");
 //<script src="https://gist.github.com/KaledGR/8f9c92206d65f860a9a4a7a5c7de6ad6.js"></script>
 https://github.com/users/KaledGR/projects/1/views/1
+hola de nuevo
